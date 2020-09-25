dde638a6845e0cdc12b71257e9c949ec3846e34b
johndev4 <johndev4@gmail.com>
2020-09-25 12:26:00 +0800

    update main and attribute

--- attributes.py
+++ attributes.py
@@ -2,28 +2,30 @@ CREATOR = "John Mistica"
 AI_ASSISTANT = "Abby"
 ME = ""
 
+HEAD = "AI Abby Virtual Assistant [Version 1.19-22alpha]\n(c) 2020, AI Abby Virtual Assistant. All Rights Reserved."
+
 #song list from YouTube
 music_list = [
-        "mXZ8-yJUpfw",
-        "nM0xDI5R50E", 
-        "rZHC1zMiWiU",
-        "tQhgEYJo8Dc",
-        "vRXZj0DzXIA",
-        "09R8_2nJtjg",
-        "32si5cfrCNc",
-        "vK7ADfHdylo",
-        "MhuDPmTOtQc",
-        "-FmVBPuZtN8",
-        "bwmSjveL3Lc",
-        "LR85RJ01OVw",
-        "8I_vvtycdyI",
-        "zrOyRF-fi8w",
-        "Vzfzf7UdIq8",
-        "7xvpY7-6kz4",
-        "tJM0yIbg8iQ",
-        "qCZAynQU_-8",
-        "z3wwWFsSlNQ",
-        "mBcqria2wmg",
-        "DXDGE_lRI0E",
-        "OUFuaMj6z-w"
-    ]
+    "mXZ8-yJUpfw",
+    "nM0xDI5R50E", 
+    "rZHC1zMiWiU",
+    "tQhgEYJo8Dc",
+    "vRXZj0DzXIA",
+    "09R8_2nJtjg",
+    "32si5cfrCNc",
+    "vK7ADfHdylo",
+    "MhuDPmTOtQc",
+    "-FmVBPuZtN8",
+    "bwmSjveL3Lc",
+    "LR85RJ01OVw",
+    "8I_vvtycdyI",
+    "zrOyRF-fi8w",
+    "Vzfzf7UdIq8",
+    "7xvpY7-6kz4",
+    "tJM0yIbg8iQ",
+    "qCZAynQU_-8",
+    "z3wwWFsSlNQ",
+    "mBcqria2wmg",
+    "DXDGE_lRI0E",
+    "OUFuaMj6z-w"
+]
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-#AI_Abby
-#version 1.19-22alpha
-#Author: John Mistica
-# Duration created: September 19-22, 2020
+# AI Abby Virtual Assistant [Version 1.19-22alpha]
+# (c) 2020, AI Abby Virtual Assistant. All Rights Reserved.
+# Author: Johndev4
+# Created: September 19-22, 2020
 
-from attributes import AI_ASSISTANT, CREATOR, ME
+from attributes import AI_ASSISTANT, CREATOR, ME, HEAD
 from voice_system import VoiceSystem
 import operations
 from get_started import get_username
 
 if __name__ == "__main__":
+    print(f"{HEAD}\r\n\r\n")
     print("Initializing the program...\r\n")
     
     voice_Sys = VoiceSystem(AI_ASSISTANT, ME)
